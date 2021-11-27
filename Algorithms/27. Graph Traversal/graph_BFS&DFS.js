1a18ae7f3cba47a258979ae051b4106526020d11
Danny Park <danincali@gmail.com>
2021-11-26 21:21:36 -0800

    graph traversals

--- Algorithms/27. Graph Traversal/graph_BFS&DFS.js
+++ Algorithms/27. Graph Traversal/graph_BFS&DFS.js
@@ -29,13 +29,13 @@ class Graph{
         const visited = {};
         const adjacencyList = this.adjacencyList;
 
-        (function dfs(vertex){
-            if(!vertex) return null;
-            visited[vertex] = true;
-            result.push(vertex);
+        (function dfs(vertex){        // helper function that accepts vertex
+            if(!vertex) return null;  // base case when vertex is empty
+            visited[vertex] = true;   // mark vertex as visited
+            result.push(vertex);      // add vertex to results list
             adjacencyList[vertex].forEach(neighbor => {
-                if(!visited[neighbor]){
-                    return dfs(neighbor)
+                if(!visited[neighbor]){   // if neighbor is not visited
+                    return dfs(neighbor)  // recursively call DFS on neighbor
                 }
             });
         })(start);
@@ -54,10 +54,31 @@ class Graph{
             result.push(currentVertex);
 
             this.adjacencyList[currentVertex].forEach(neighbor => {
-               if(!visited[neighbor]){
-                   visited[neighbor] = true;
-                   stack.push(neighbor)
+               if(!visited[neighbor]){        // check if the vertex hasn't been visited
+                   visited[neighbor] = true;  // mark it as visited
+                   stack.push(neighbor)       // add vertex to the result list; will end on the end (when no vertex to push)
                } 
+            });
+        }
+        return result;
+    }
+    breadthFirst(start){
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+           
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
             });
         }
         return result;
@@ -82,6 +103,13 @@ g.addEdge("D","E")
 g.addEdge("D","F")
 g.addEdge("E","F")
 
+console.log(g);
+console.log("DFS recursive:")
+console.log(g.depthFirstRecursive("A"));
+console.log("DFS iterative:")
+console.log(g.depthFirstIterative("A"));
+console.log("BFS:")
+console.log(g.breadthFirst("A"));
 
 //          A
 //        /   \
@@ -90,4 +118,7 @@ g.addEdge("E","F")
 //       D --- E
 //        \   /
 //          F
+
+// QUEUE: []
+// RESULT: [A, B, C, D, E, F]
 
