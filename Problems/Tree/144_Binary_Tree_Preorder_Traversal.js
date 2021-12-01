bad277308bce954d2bee086909014a833a9f088f
Danny Park <danincali@gmail.com>
2021-12-01 14:23:49 -0800

    leetcode-bst

---
+++ Problems/Tree/144_Binary_Tree_Preorder_Traversal.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var preorderTraversal = function(root) {
+    if(!root) return [];
+    
+    // result array 
+    let result = [];
+    // stack array that queues available nodes in pre-order
+    let stack = [root];
+    
+    // exit if no node is left to push to queue
+    while(stack.length){
+        let node = stack.pop();
+        result.push(node.val);
+        
+        if(node.left){
+            stack.push(node.left);
+        }
+        if(node.right){
+            stack.push(node.right);
+        }
+    }
+    
+    
+    return result;
+};
