7495f7377b8f6108616b9c09f5417c31e17e1e39
Danny Park <danincali@gmail.com>
2021-12-11 15:44:18 -0800

    graph notes

--- Algorithms/26. Graphs/graph_remove_vertex.js
+++ Algorithms/26. Graphs/graph_remove_vertex.js
@@ -25,31 +25,31 @@
 
 
 class Graph{
-    constructor(){
-        this.adjacencyList = {};
+  constructor(){
+    this.adjacencyList = {};
+  }
+  addVertex(vertex){
+    if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1,v2){
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(vertex1,vertex2){
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+  }
+  removeVertex(vertex){
+    while(this.adjacencyList[vertex].length){
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
     }
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-    }
-    addEdge(v1,v2){
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
-    }
-    removeEdge(vertex1,vertex2){
-        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-            v => v !== vertex2
-        );
-        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-            v => v !== vertex1
-        );
-    }
-    removeVertex(vertex){
-        while(this.adjacencyList[vertex].length){
-            const adjacentVertex = this.adjacencyList[vertex].pop();
-            this.removeEdge(vertex, adjacentVertex);
-        }
-        delete this.adjacencyList[vertex]
-    }
+    delete this.adjacencyList[vertex]
+  }
 }
 
 let g = new Graph();
