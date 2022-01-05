83754109c15e0c3c0ffab6b3c4f883fa31cf2b50
Danny Park <danincali@gmail.com>
2022-01-04 21:16:04 -0800

    tree

--- Algorithms/23. Tree Traversal/Breadth_FIrst_Tree.js
+++ Algorithms/23. Tree Traversal/Breadth_FIrst_Tree.js
@@ -88,18 +88,18 @@ class BinarySearchTree {
   }
 
   BFS(){
-  let node = this.root,
-    data = [],
-    queue = [];
-  queue.push(node);
+    let node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
 
-  while(queue.length){
-    node = queue.shift();
-    data.push(node.value);
-    if(node.left) queue.push(node.left);
-    if(node.right) queue.push(node.right);
-  }
-  return data;
+    while(queue.length){
+      node = queue.shift();
+      data.push(node.value);
+      if(node.left) queue.push(node.left);
+      if(node.right) queue.push(node.right);
+    }
+    return data;
   }
 }
 
