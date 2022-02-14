cfdd1482f9096998488b681d915175d9e34ff05c
Danny Park <danincali@gmail.com>
2022-02-13 17:57:41 -0800

    reverse linked list"

---
+++ Problems/Linked-Lists/24_ Swap Nodes in Pairs.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var swapPairs = function(head) {
+  if(!head || !head.next) return head;
+
+  let v1 = head, v2 = head.next, v3 = v2.next;
+  v2.next = v1;
+  v1.next = swapPairs(v3);
+  
+  return v2; 
+};
